6220654c3ead70919e3b8d2832408573f0139355
allan6757 <allanoomaina@gmail.com>
2025-06-29 00:24:14 +0300

    Complete Arithmetic Lab: math operators and built-in objects

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // 62
+
+const random = Math.floor(Math.random() * 100) + 1; // random integer > 0
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // 4
+
+const max = Math.max(5, 10, 15, 20); // 20
